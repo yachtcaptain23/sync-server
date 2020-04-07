703acbc66773c143949c8caa094274091324fbb2
Jocelyn Liu <yrliou@gmail.com>
2020-04-07 13:38:47 -0700

    Modify types_test to use utils package

--- utils/types_test.go
+++ utils/types_test.go
@@ -1,31 +1,30 @@
-package utils_test
+package utils
 
 import (
 	"database/sql"
 	"testing"
 
-	"github.com/brave-experiments/sync-server/utils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStringOrNull(t *testing.T) {
 	// Passing nil should returns invalid sql.NullString.
-	assert.False(t, utils.StringOrNull(nil).Valid)
+	assert.False(t, StringOrNull(nil).Valid)
 
 	// Passing a normal string pointer should returns a valid sql.NullString.
 	v := "test"
-	assert.Equal(t, utils.StringOrNull(&v), sql.NullString{String: v, Valid: true})
+	assert.Equal(t, StringOrNull(&v), sql.NullString{String: v, Valid: true})
 }
 
 func TestStringPtr(t *testing.T) {
 	// Invalid sql.NullString should returns nil.
 	nullString := &sql.NullString{String: "", Valid: false}
-	assert.Nil(t, utils.StringPtr(nullString))
+	assert.Nil(t, StringPtr(nullString))
 
 	// Valid sql.NullString should returns a pointer to the string value.
 	v := "test"
 	nullString = &sql.NullString{String: v, Valid: true}
-	out := utils.StringPtr(nullString)
+	out := StringPtr(nullString)
 	assert.NotNil(t, out)
 	assert.Equal(t, *out, v)
 }
